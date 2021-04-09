aa9c3bea2ff28cccc3f793f81a0059902c8ad165
Luciano Silva <luciano2023silva@gmail.com>
2021-04-09 12:58:40 -0300

    create Dashboard and Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,13 +5,13 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #e52e4d;
         --blue: #5429cc;
+        --green: #33cc95;
 
         --blue-light: #6933ff;
 
         --text-title: #363f3f;
         --text-body: #969cb3;
 
-        --background: #f0f2f5;
         --shape: #ffffff;
     }
 
